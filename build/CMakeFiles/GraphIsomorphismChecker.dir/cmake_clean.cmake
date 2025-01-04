798458149abb1a797d146ca4e07eee561cd7003d
loui263 <louie2201aa@gmail.com>
2025-01-04 23:30:10 +0100

    aaz

---
+++ build/CMakeFiles/GraphIsomorphismChecker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphIsomorphismChecker.dir/main.cpp.o"
+  "CMakeFiles/GraphIsomorphismChecker.dir/main.cpp.o.d"
+  "GraphIsomorphismChecker"
+  "GraphIsomorphismChecker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphIsomorphismChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
