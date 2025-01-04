d99420d53fa7329449f064936e301de8580a16be
loui263 <louie2201aa@gmail.com>
2025-01-04 18:56:11 +0100

    az

--- mingw-toolchain.cmake
+++ mingw-toolchain.cmake
@@ -1,13 +1,10 @@
-# Custom toolchain file for cross-compiling to Windows
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_VERSION 1)
 
-# Specify the cross-compilers
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
-# Set the root path for finding libraries and includes
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
